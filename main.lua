5d3a501e5ce7fdd67e1ec198b20b6f5338f06411
Le Duy Anh <lenduya@gmail.com>
2018-09-21 19:01:38 +0200

    Add dt value

--- main.lua
+++ main.lua
@@ -13,8 +13,8 @@ function love.load()
     player1.setPos(300, 300)
 end
 
-function love.update()
-    movePlayer(player1)
+function love.update(dt)
+    movePlayer(player1, dt)
 end
 
 function love.draw()
@@ -22,32 +22,33 @@ function love.draw()
     love.graphics.draw(player1.sprite, player1.x, player1.y)
 end
 
-function move(object, direction)
+function move(object, direction, dt)
+    relativeSpeed = object.speed * dt * 60
     if direction == 'l' then
-        object.x = object.x - object.speed
+        object.x = object.x - relativeSpeed
     end
     if direction == 'r' then
-        object.x = object.x + object.speed
+        object.x = object.x + relativeSpeed
     end
     if direction == 'u' then
-        object.y = object.y - object.speed
+        object.y = object.y - relativeSpeed
     end
     if direction == 'd' then
-        object.y = object.y + object.speed
+        object.y = object.y + relativeSpeed
     end
 end
 
-function movePlayer(player)
+function movePlayer(player, dt)
     if love.keyboard.isDown('e') then
-        move(player, 'u')
+        move(player, 'u', dt)
     end
     if love.keyboard.isDown('d') then
-        move(player, 'd')
+        move(player, 'd', dt)
     end
     if love.keyboard.isDown('s') then
-        move(player, 'l')
+        move(player, 'l', dt)
     end
     if love.keyboard.isDown('f') then
-        move(player, 'r')
+        move(player, 'r', dt)
     end
 end
--- player.lua
+++ player.lua
@@ -2,23 +2,13 @@
 local Player =  {
     x = 0,
     y = 0,
-    speed = 1,
+    speed = 3,
     sprite = love.graphics.newImage('sprites/player.png')
 }
 
 function Player.setPos(x, y)
     Player.x = x
     Player.y = y
-end
-
-function Player.moveUp()
-    Player.y = Player.y - 1
-end
-function Player.moveDown()
-    Player.y = Player.y + 1
-end
-function Player.moveLeft()
-    Player.x = Player.x - 1
 end
 
 return Player
