16d49f09e04aeb41bf071e88e0713a9e95abb6f3
Le Duy Anh <lenduya@gmail.com>
2018-09-21 19:35:29 +0200

    Player now rotates according to mouse

--- main.lua
+++ main.lua
@@ -15,40 +15,10 @@ end
 
 function love.update(dt)
     player1.move(dt)
+    player1.rotate(dt)
 end
 
 function love.draw()
     love.graphics.draw(sprites.background, 0, 0)
-    love.graphics.draw(player1.sprite, player1.x, player1.y)
+    love.graphics.draw(player1.sprite, player1.position.x, player1.position.y, player1.angle, nil, nil, player1.sprite:getWidth()/2, player1.sprite:getHeight()/2)
 end
-
--- function move(object, direction, dt)
---     relativeSpeed = object.speed * dt * 60
---     if direction == 'l' then
---         object.x = object.x - relativeSpeed
---     end
---     if direction == 'r' then
---         object.x = object.x + relativeSpeed
---     end
---     if direction == 'u' then
---         object.y = object.y - relativeSpeed
---     end
---     if direction == 'd' then
---         object.y = object.y + relativeSpeed
---     end
--- end
-
--- function movePlayer(player, dt)
---     if love.keyboard.isDown('e') then
---         move(player, 'u', dt)
---     end
---     if love.keyboard.isDown('d') then
---         move(player, 'd', dt)
---     end
---     if love.keyboard.isDown('s') then
---         move(player, 'l', dt)
---     end
---     if love.keyboard.isDown('f') then
---         move(player, 'r', dt)
---     end
--- end
--- player.lua
+++ player.lua
@@ -1,30 +1,34 @@
 
 local Player =  {
-    x = 0,
-    y = 0,
+    position = {},
     speed = 3,
+    angle = 0,
     sprite = love.graphics.newImage('sprites/player.png')
 }
 
 function Player.setPos(x, y)
-    Player.x = x
-    Player.y = y
+    Player.position.x = x
+    Player.position.y = y
 end
 
 function Player.move(dt)
     distance = Player.speed * dt * 60
     if love.keyboard.isDown('e') then
-        Player.y = Player.y - distance
+        Player.position.y = Player.position.y - distance
     end
     if love.keyboard.isDown('d') then
-        Player.y = Player.y + distance
+        Player.position.y = Player.position.y + distance
     end
     if love.keyboard.isDown('s') then
-        Player.x = Player.x - distance
+        Player.position.x = Player.position.x - distance
     end
     if love.keyboard.isDown('f') then
-        Player.x = Player.x + distance
+        Player.position.x = Player.position.x + distance
     end
+end
+
+function Player.rotate(dt) 
+    Player.angle = math.atan2(love.mouse.getY() - Player.position.y, love.mouse.getX() - Player.position.x)
 end
 
 return Player
