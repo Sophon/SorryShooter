0536b7b9ca34561c29c9fde9e32abd87295b48ae
Le Duy Anh <lenduya@gmail.com>
2018-10-06 20:42:01 +0200

    Extract update to Update()

--- main.lua
+++ main.lua
@@ -1,11 +1,10 @@
 
-
-
 local Bullet = require("bullet")
 local Player = require("player")
 local Zombie = require("zombie")
 
 require("load")
+require("update")
 
 hitbox = 20
 screenW = love.graphics.getWidth()
@@ -18,50 +17,7 @@ function love.load()
 end
 
 function love.update(dt)
-    player1:move(dt)
-    player1:rotate()
-
-    --TODO: extract some into functions?
-    for i, z in ipairs(zombies) do
-        z:rotate(player1)
-        z:move(dt)
-        if distance(player1, z) <= hitbox then 
-            killAllZombies()
-        end
-    end
-
-    for i, b in ipairs(bullets) do
-        b:move(dt)
-    end
-
-    for i = #bullets, 1, -1 do 
-       local b = bullets[i]
-       if b.pos.x < 0 or b.pos.y < 0 or b.pos.x > screenW or b.pos.y > screenH then
-            table.remove(bullets, i)
-       end
-    end
-
-    for i, z in ipairs(zombies) do
-        for j, b in ipairs(bullets) do
-            if distance(b, z) <= hitbox then
-                b.dead = true
-                z.dead = true
-            end
-        end
-    end
-
-    for i = #bullets, 1, -1 do
-        local b = bullets[i]
-        if b.dead == true then
-            table.remove(bullets, i)
-        end
-    end
-    for i = #zombies, 1, -1 do
-        local z = zombies[i]
-        if z.dead == true then
-            table.remove(zombies, i)
-        end
-    end
+    Update(dt)
 end
 
 function love.draw()
